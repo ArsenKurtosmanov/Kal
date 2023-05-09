502ba69dd741b3e27a7279256b514d5e9a516bc4
999 <999@DEALDENTON>
2023-05-09 23:09:33 +0300

    Добавьте файлы проекта.

---
+++ Kal/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Kal
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button8 = new System.Windows.Forms.Button();
+            button9 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            textBox1.Location = new System.Drawing.Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(128, 27);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(12, 140);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(94, 29);
+            button1.TabIndex = 25;
+            button1.Text = "7";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += Buttons_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(112, 140);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(94, 29);
+            button2.TabIndex = 26;
+            button2.Text = "8";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += Buttons_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(212, 140);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(94, 29);
+            button3.TabIndex = 27;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += Buttons_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(12, 175);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(94, 29);
+            button4.TabIndex = 28;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += Buttons_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new System.Drawing.Point(112, 175);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(94, 29);
+            button5.TabIndex = 29;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += Buttons_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new System.Drawing.Point(212, 175);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(94, 29);
+            button6.TabIndex = 30;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new System.Drawing.Point(12, 210);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(94, 29);
+            button7.TabIndex = 31;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new System.Drawing.Point(112, 210);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(94, 29);
+            button8.TabIndex = 32;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new System.Drawing.Point(212, 210);
+            button9.Name = "button9";
+            button9.Size = new System.Drawing.Size(94, 29);
+            button9.TabIndex = 33;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(394, 445);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
